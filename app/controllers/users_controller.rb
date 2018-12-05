d93548d341cc13825cf0c1426dcafb6caab7b0b6
SylvainJunca <sylvain.junca@gmail.com>
2018-12-05 18:47:14 -0500

    updates routes and application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,10 @@ class UsersController < ApplicationController
     end
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
 end
