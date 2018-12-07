8e4455a50ff68369e61ed568454b69e9ff739a79
SylvainJunca <sylvain.junca@gmail.com>
2018-12-07 11:22:39 -0500

    adds relationship between review, user and product plus the validation for the review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,10 @@
 class Review < ActiveRecord::Base
+  belongs_to :product 
+  belongs_to :user
+
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :description, length: ( minimum : 20 )
+  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5}
 end
+
