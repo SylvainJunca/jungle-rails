adcafde32f85652cdb26fbcc0b2c6a11c33883f6
SylvainJunca <sylvain.junca@gmail.com>
2018-12-07 16:04:11 -0500

    now we can post reviews !!!! Yay ! I don't know how I made this work, but it works !!!

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,18 @@
 class ReviewsController < ApplicationController
 
   def create
-    raise "Yay, I'm here!"
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
+    if @review.save
+      redirect_to product_path(@product)
+    else
+      render :new
+    end
   end
+
+  private
+    def review_params
+      params.require(:review).permit(:description, :rating)
+    end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ActiveRecord::Base
   belongs_to :user
 
   validates :product_id, presence: true
-  # validates :user_id, presence: true
+  validates :user_id, presence: true
   validates :description, presence: true
   validates :rating, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
 end
